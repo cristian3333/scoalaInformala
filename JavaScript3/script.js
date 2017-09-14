aeca756e7cc5c8dc9f16eb26a449d66cec641fc5
cristianDragu <dragu.cristiann@yahoo.com>
2017-09-14 23:34:27 +0300

    js3

--- JavaScript3/script.js
+++ JavaScript3/script.js
@@ -1,258 +1,232 @@
-function rest(a,b){
-   
-    var c=  a %  b;
-    return c;
-}
 
-console.log(rest(7,5));
-console.log(Math.PI);
-// ------------------START COMPARE 2 STRINGS----------------------------------
-"abc 2"
-"abc 12"
+// console.log("a is grater than b : " + f3("abc 2", "abc 12"));
 
-// function sort(a,b){
-        
+//////////////////////////////////////=======================DOOM---------------------------------------------------------------
 
-//     for(var i=0; i< a.length || i < b.length; i++){
-          
-//         var = 1;
+// document.getElementById("moloz").innerHTML = "alt <br> moloz";
 
-//         while(a[i] >= "a" && a[i] <="z" && b[i] >= 0 && b[i] <=9 || a[i] > b[i]){
-//             b[i] += b[i+1];
-//             return true;
-//         }
-//     }
+// document.getElementById("altParagraf").innerText = "alt <br>  moloz";
 
+// var myDivs = document.getElementsByClassName("fancy");
+
+// for(var i=0; i<myDivs.length; i++){
+//     myDivs[i].innerHTML = myDivs[i].innerHTML + "it works" + " at " + Date();
 // }
 
-// console.log("a is grater than b : " + sort("abc 2", "abc 12"));
+// .innerHtml OR  .style ----------------------------------css------------------
+// document.getElementById("moloz").style.color = "black";
+// document.getElementById("altParagraf").style.backgroundColor = "red";
 
-function f3(a, b){
-    var i = 0;
-    while(a[i] >= "a" && a[i] <= "z" && b[i] >= "a" && b[i] <= "z"){
-        if (a[i] > b[i]) return -1;
-            else if(a[i] < b[i]) return 1;
-        i++;
-    }
-    if (a[i] >= "a" && a[i] <= "z" && !(b[i] >= "a" && b[i] <= "z")) return -1;
-    if (!(a[i] >= "a" && a[i] <= "z") && b[i] >= "a" && b[i] <= "z") return 1;
+// for img -  WHICH HAS NO CLOSING TAG ---------------------
 
-    var n1 = 0;
-    var n2 = 0;
-    while(a[i] && b[i]){
-        n1 = n1*10 + a[i];
-        n2 = n2*10 + b[i];
-        i++;
-    }
-    if (a[i]) {return -1;}
-        else if(b[i]) {return 1;}
-    if (n1 > n2) return -1;
-        else if(n1 < n2) return 1;
-    return 0;
-}
+// document.getElementById("altParagraf").getAttribute("src");
 
-console.log("a is grater than b : " + f3("abc 2", "abc 12"));
-// ------------------STOP COMPARE 2 STRINGS------------------------------------------------------------------------------------------
+// document.getElementById("altParagraf").setAttribute("src", "");
+// document.getElementById("altParagraf").src = ("src");
+//------------------------------------------------------------------------------------------------------------------------------------------
+// .classList.add();
+// .classList.remove();
 
-// ---------------------NR1-FACTORIAL FOR A  NUMBER--------------------------------------------------
-function factorial(n){
 
-    var x=1;
-    for(i=1; i<=n; i++){
-         x= i*x
-    }
-    return x;
-}
-console.log("The factorial number : " + factorial("5"));
+// .querySelector("");
+// .querySelectorAll("#test .fancy span");
 
-//------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------
 
-// ---------------NR2 - CMMDC - GCD ---------------------------
-//----------------------------------START Nr.2 - GCD -->
-  
-function gcd(a,b){
-    if(b==0){
-      return a
-  }
-  else if(a>b){
-      return gcd(b, a%b);    
-  }
-  else{
-  return gcd(a, b%a);
-  } 
-}
+// function changeColor(){
+//     document.getElementById("moloz").style.backgroundColor="green";
 
-//----------------------------------STOP Nr.2 - GCD -->
-//----------------------------------START Nr.3 - LCM -->
-function lcm(a,b){
-    return (a*b)/gcd(a,b) ;  
-}
-  console.log("The GCD of a and b is : " + gcd(210,45));
-  console.log("The LCM of a and b is : " + lcm(210,45));
-  
-    //----------------------------------STOP Nr.3 - LCM -->
-// -------- NR 4 - START - ALL DIVISORS IN ARRAY----------
-
-function divisors(a){
-    var arr=[];
-    for (i=2; i<a; i++){
-        if(a % i == 0){
-            arr.push(i);
-        }
-    }
-    return arr;
-}
-console.log(divisors(64));
-// -------- NR 4 - STOP - ALL DIVISORS IN ARRAY----------
-
-function divisorsP(a){
-    var arr=[];
-    for (i=2; i<=a;){
-        if(a % i == 0){
-            arr.push(i);
-            a = a/i;
-        }else{
-            i++;
-        }
-    }
-    return arr;
-}
-console.log(divisorsP(64));
-    //-----NR 5 START - ALL PRIME DIVISORS------------
-// function allPrime(integer){
-//     var primeArray = [];
-//     var isPrime = false;
+// }
+// function changeColor2(){
+//     document.getElementById("moloz").style.backgroundColor="red";
     
-//     //find divisors starting with 2
-    
-//     for(i = 1; i <= integer; i++){
-//     if (integer % i==0) {
-    
-//      //check if divisor is prime    
-//      for(var j = 1; j <= i/1; j++) {
-//        if(i % j == 0) {
-//          isPrime = false;
-//        } else {
-//          isPrime = true;
-//        }
-//      }
-    
-//      //if divisor is prime
-    
-//         if (isPrime == true) {
-//         //divide integer by prime factor & factor store in array primeArray
-//         i = integer / i ;
-//         primeArray.push(i);
-//         }
-//     }   
-    
-//     }
-//     for (var k = 0; k < primeArray.length; k++) {
-//         console.log(primeArray[k]);
-//         }
-//     return primeArray;
 // }
 
-// console.log(allPrime(64));
 
-function allPrime(n){
-    var i=2;
-    while(i <= n){
-        if(n%i == 0){
-            n/=i;
-            // console.log(i);
-            // console.log(n);
-        }else{
-            i++;
-            // console.log(i);
-        }
+//------------------------------------------------------------------------------------------------------------------------------------------
+// document.querySelector('#myForm [name ="firstName"]');
+
+// var firstNameElem = document.querySelector('#myForm [name ="firstName"]');
+// var lastNameElem = document.querySelector('#myForm [name ="lastName"]');
+
+// alert(firstNameElem.value);
+
+// function keyDown(){
+//     console.log("keyDown", firstNameElem.value);
+// }
+
+// function keyUp(){
+//     console.log("keyUp", firstNameElem.value);
+// }
+// function keyPress(){
+//     console.log("keyPress", firstNameElem.value);
+// }
+
+// function change(){
+//     console.log("change", firstNameElem.value);
+// }
+//------------------------------------------------------------------------------------------------------------------------------------------
+// function input(event, elem){
+//     console.log("input", elem.value);
+//     document.getElementById("hello").innerHTML = "Servus" + " "+ elem.value  + " !";
+// }
+ 
+// or
+
+// function input(event){
+//     // console.log("input", elem.value);
+//     document.getElementById("hello").innerHTML = "Servus" + " " + firstNameElem.value + " "+ lastNameElem.value + " !";
+// }
+//------------------------------------------------------------------------------------------------------------------------------------------
+// function add(event){
+//     var firstNumber = document.querySelector('#myForm [name ="firstNumber"]');
+//     var secondNumber = document.querySelector('#myForm [name ="secondNumber"]');
+
+//     var add = parseInt(firstNumber.value) + parseInt(secondNumber.value);
+
+//     document.getElementById("sum").value = add;
+
+    // if( isNaN(parseInt(firstNumber.value))){
+    //     firstNumber.value = firstNumber.value + 0;
+    // }
+    // else if( isNaN(parseInt(secondNumber.value))){
+    //     secondNumber.value = secondNumber.value + 0;
+    // }
+// }
+
+
+
+
+function check(event){
+    var firstValue = document.querySelector('#firstForm [name = "firstValue"]');
+    var secondValue = document.querySelector('#firstForm [name = "secondValue"]');
+
+    if(parseInt(firstValue.value) > parseInt(secondValue.value)){
+        firstValue.style.color = "green";
+        secondValue.style.color = "red";
+
+    }else if(parseInt(firstValue.value) === parseInt(secondValue.value)){
+        firstValue.style.color = "green";
+        secondValue.style.color = "green";
+
+    }else if(parseInt(firstValue.value) < parseInt(secondValue.value)){
+        secondValue.style.color = "green";
+        firstValue.style.color = "red";
+
+    }else{
+        firstValue.style.color = "red";
+        secondValue.style.color = "red";
     }
-    return i;
-// console.log(i);    
+    
 }
-var a= 64;
-console.log(allPrime(a));
 
+function check2(event){
+    var firstValue = document.querySelector('#secondForm [name = "firstValue"]');
+    var secondValue = document.querySelector('#secondForm [name = "secondValue"]');
 
-//-----N3 5 STOP - ALL PRIME DIVISORS------------
+    var add = parseInt(firstValue.value) + parseInt(secondValue.value);
 
-// -------- NR 6 - START - ALL UNIQUE PRIME DIVISORS IN ARRAY----------
-
-// -------- NR 6 - STOP - ALL UNIQUE PRIME DIVISORS IN ARRAY----------
-
-//-------------NR 7 -----------------------
-
-function LengthConverter(a) {
-    return a/0.62137;
-  }
-  console.log("KM from Miles: "  + LengthConverter(10));
-  
-  function gallonConverter(a) {
-    return a*3.78541178; 
-  }
-  console.log("Litres from Gallons: "  + gallonConverter(10));
-   
-   //-------------NR 8 Celsius to F -----------------------
-   
-   function fConverter(a) {
-    return (a*1.8)+32; 
-  }
-  console.log("F from Celsius: "  + fConverter(10));
-  
-  //--------------Nr 9 Degrees in Radians----------
-  
-   function fConverter(a) {
-    return (a*0.01745329252) 
-  }
-  console.log("Radians from Degrees: "  + fConverter(10));
-  
-  //--------NR 10 - Suplement of an angle-------------
-  
-   function suplement(a) {
-    return (180 - a) 
-  }
-  console.log("Suplement of a "  + suplement(10));
-   
-   //--------NR 11 - Complement of an angle-------------
-   
-    function complement(a) {
-    return (90 - a) 
-  }
-  console.log("Complement of a "  + complement(10));
-   
-  // NR 12 --- Array + array-----------
-  
-    function arrays(a,b) {  
-     return a + ',' + b; 
-  }
-  console.log("The array is "  + arrays([10,11,12],[13,14,15]));
-
-
-  // NR 13 ---------------   RETURN REVERSE------------------
-
-  function reverse(a){
+    document.getElementById("sum2").value = add;
     
-        var c = '';
+}
+
+function check3(event){
+    var firstValue = document.querySelector('#thirdForm [name = "firstValue"]');
+    var secondValue = document.querySelector('#thirdForm [name = "secondValue"]');
+
+    if(parseInt(firstValue.value) > parseInt(secondValue.value)){
+        document.getElementById("bigger").value = firstValue.value;
+
+    }else if(parseInt(firstValue.value) === parseInt(secondValue.value)){
+        document.getElementById("bigger").value = "They are equal."
+
+    }else if(parseInt(firstValue.value) < parseInt(secondValue.value)){
+        document.getElementById("bigger").value = secondValue.value;
+
+    }    
+}
+
+function check4(event){
+    var firstValue = document.querySelector('#fourthForm [name = "firstValue"]');
+    var limit  = firstValue.value.length;
     
-        for(var i=a.length-1; i>=0; i--){
-            c+= a[i]
-        }
-        if(c === a ){
-             console.log('yeeeeeeeeeeey')
-        }else{
-            console.log('booooooooooooo')
-        }
-        return c;
+    document.getElementById("length").innerHTML = "The length of the input : " + "<p id =\"alertLimit\">" + limit + "</p>";
+    
+    if (limit >= 10){
+        document.getElementById("alertLimit").style.color = "red";
+        // firstValue.addEventListener("keypress", function(event){
+        //     event.preventDefault()
+        // });
+    }
+    // else{
+
+    // }
+    
+}
+
+
+function check5(event){
+    var firstValue = document.querySelector('#fifthForm [name = "firstValue"]');
+    
+    var firstValueP = parseInt(firstValue.value);
+
+    if(event.key >= "a" || event.key <= "z"){
+        firstValue.addEventListener("keypress", function(event){
+            event.preventDefault()
+        });
+    }else{
+
     }
     
-    console.log(reverse("anasana"));
+}
 
-    // NR 14 ---------------   Check Palindrome ------------------
 
-    function checkPalindrome(a){
-        
-    return a == a.split('').reverse().join('');
-    }
-    
 
-console.log(checkPalindrome("anssna"));
+
+// function add(event){
+//     var firstNumber = document.querySelector('#myForm [name ="firstNumber"]');
+//     var secondNumber = document.querySelector('#myForm [name ="secondNumber"]');
+
+//     var add = parseInt(firstNumber.value) + parseInt(secondNumber.value);
+
+//     document.getElementById("sum").value = add;
+
+    // if( isNaN(parseInt(firstNumber.value))){
+    //     firstNumber.value = firstNumber.value + 0;
+    // }
+    // else if( isNaN(parseInt(secondNumber.value))){
+    //     secondNumber.value = secondNumber.value + 0;
+    // }
+// }
+
+
+function check6(event, elem){
+    // document.getElementById("altParagraf").getAttribute("src");
+
+// document.getElementById("altParagraf").setAttribute("src", "");
+    elem.src = ("http://www.drodd.com/images14/x12.jpg");
+    console.log("mouseover", elem);
+    console.log("mouseover", elem.value);
+
+}
+
+function check7(event, elem){
+
+    elem.src = ("http://static.wixstatic.com/media/b77fe464cfc445da9003a5383a3e1acf.jpg");
+    console.log("mouseout", elem);
+    console.log("mouseout", elem.value);
+
+}
+
+function check8(event, elem){
+
+    elem.src = ("http://www.drodd.com/images14/x12.jpg");
+    console.log("click", elem);
+    console.log("click", elem.value);
+
+    elem.addEventListener("mouseout", function(event){
+        event.preventDefault();
+        event.stopPropagation();
+    });
+ 
+}
